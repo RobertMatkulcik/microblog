43c1860fee24e808ca9ff8b0b3b48bb819df0486
Robert Matkulcik <robert@matkulcik.eu>
2016-11-04 01:38:14 +0100

    initialize git reppo & flask login + DB on work

---
+++ db_create.py
@@ -0,0 +1,13 @@
+#!flask/bin/python
+# All the application specific pathnames are imported from the config file. So I can use this for all projects
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
